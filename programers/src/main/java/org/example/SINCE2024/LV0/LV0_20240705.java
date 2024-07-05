fe06a04686bb4ebe6fca745b452f18727b3a7d2d
jonghyun <goste390@gmail.com>
2024-07-05 22:18:47 +0900

    정말 어려운 문자열을 정수로 변환하기

---
+++ programers/src/main/java/org/example/SINCE2024/LV0/LV0_20240705.java
@@ -0,0 +1,24 @@
+package org.example.SINCE2024.LV0;
+
+public class LV0_20240705 {
+    public static void main(String[] args) {
+
+        String n_str1 = "10";//	10
+        String n_str2 = "8542";//	8542]
+
+        System.out.println(solution1(n_str1));
+        System.out.println(solution1(n_str2));
+
+    }
+
+
+    public static int solution1(String n_str) {
+        int answer = 0;
+
+        answer = Integer.valueOf(n_str);
+
+        return answer;
+    }
+
+
+}
