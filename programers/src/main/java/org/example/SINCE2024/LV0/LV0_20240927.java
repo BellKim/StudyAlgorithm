84644577bfab8304c89058a4a3f23a109c29a379
jonghyun <goste390@gmail.com>
2024-09-27 23:34:19 +0900

    홀수와 짝수!

---
+++ programers/src/main/java/org/example/SINCE2024/LV0/LV0_20240927.java
@@ -0,0 +1,60 @@
+package org.example.SINCE2024.LV0;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LV0_20240911 {
+    public static void main(String[] args) {
+
+        int[] num_list1 = {4, 2, 6, 1, 7, 6};//	17
+        int[] num_list2 = {-1, 2, 5, 6, 3}; //	8
+
+
+        System.out.println(solution1(num_list1));
+        System.out.println(solution1(num_list2));
+
+
+    }
+
+    public static void printarr(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + ", ");
+        }
+        System.out.println();
+    }
+
+
+    /* https://school.programmers.co.kr/learn/courses/30/lessons/181887
+        홀수 vs 짝수
+        num_list 라는 배열이 주어지는데 홀수 번째 숫자와 짝수번째 숫자와 구분지어서 홀수그룹 짝수그릅의 수를 각각 더한뒤 큰 수를 리턴한다.
+
+        다른사람들의 풀이를 보면 생각치 못한 풀이들이 간혹 보이기도 하는데
+        이번에는 특별한 느낌의 풀이를 보진 못헀다.
+
+
+     */
+    public static int solution1(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if(i%2 == 0) {
+                even += num_list[i];
+            }else{
+                odd += num_list[i];
+            }
+        }
+
+        if(even > odd) {
+            return even;
+        }else{
+            return odd;
+        }
+    }
+
+
+
+
+}
+
